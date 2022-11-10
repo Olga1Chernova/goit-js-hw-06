b394afa036d1c14ce9030723b81b8590bfab7a4a
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2022-11-10 02:00:14 +0200

    task 8 fixed

--- js/task-08.js
+++ js/task-08.js
@@ -1,21 +1,15 @@
-const formRef = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
 
-formRef.querySelector.addEventListener('submit', onFormSubmit);
-
-function onFormSubmit(event) {
-    event.preventDefault();
-    const targetFormEl = event.currentTarget;
-
+function handleSubmit(event) {
+  event.preventDefault();
   const {
-    email: { value: email },
-    password: { value: password },
-  } = targetFormEl.elements;
-
-  if (email.length === 0 || password.length === 0)
-    return window.alert("Please, fill out all the fields!");
-
-  console.log({ email, password });
-
-  targetFormEl.reset();
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill out all the fields!");
+  }
+  console.log({ Login: email.value, Password: password.value });
+  event.currentTarget.reset();
 }
